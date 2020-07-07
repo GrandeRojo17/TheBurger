1008181fa0905212739ebbc1c53e6acbb6c9745b
GrandeRojo17 <38540605+GrandeRojo17@users.noreply.github.com>
2020-07-07 16:01:11 -0500

    Adding final impovements

--- config/connection.js
+++ config/connection.js
@@ -2,15 +2,23 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 var util = require('util');
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "burger_db"
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'burger_db'
+  })
+  //database: "burger_db"
 
-// Make connection.
+}
+
+
+// Make connection. 
 connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
